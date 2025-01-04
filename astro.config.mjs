f646d21da3170b69004a40c6d414c1669e8d4b33
zombie <claney2005@protonmail.com>
2025-01-03 20:47:18 -0600

    Fix for discord invite and staff app redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,14 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://thekrew.network',
+    redirects: {
+        '/discord': {
+            status: 301,
+            destination: 'https://discord.gg/hdN2M5rnr3',
+        },
+        '/apply': {
+            status: 301,
+            destination: 'https://forms.gle/TFBiWd2Yev68h1Ea6',
+        }
+    }
 })
